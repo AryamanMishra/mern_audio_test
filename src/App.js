d47e4cbab4515fcc6ca25ef789fd6732d8b1023b
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2023-01-25 17:31:59 +0530

    audio recieved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import {GrPlayFill,GrPauseFill} from 'react-icons/gr'
+import MicRecorder from 'mic-recorder-to-mp3';
+// import useAudio from './useAudio'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=> {
+
+	const [Mp3Recorder] = useState(new MicRecorder({ bitRate: 128 }));
+
+	const [isRecording,setIsRecording] = useState(false)
+	const [isBlocked,setIsBlocked] = useState(true)
+
+
+	// const getUserPermission = ()=> {
+	// 	navigator.mediaDevices.getUserMedia({audio:true})
+	// 		.then(()=> {
+	// 			console.log('Permission granted')
+	// 			setIsBlocked(false)
+	// 		})
+	// 		.catch(()=> {
+	// 			console.log('Permission denied')
+	// 			setIsBlocked(true)
+	// 		})
+	// }
+
+	const startRecording = ()=> {
+		// getUserPermission()
+		setIsRecording(true)
+		Mp3Recorder
+			.start()
+			.then(()=> {
+				setIsRecording(true)
+			})
+			.catch((err)=> {
+				console.log(err)
+			})
+	}
+
+	const stopRecording = ()=> {
+		setIsRecording(false)
+		Mp3Recorder
+			.stop()
+			.getMp3()
+			.then(([buffer,blob]) => {
+				const file = new File(buffer, 'audiorec.mp3', {
+					type: blob.type,
+					lastModified: Date.now()
+				});	
+				
+				console.log(file)
+			})
+			.catch((err)=> {
+				console.log(err)
+			})
+	}
+
+	return (
+		<main>
+			<div className='main-body'>
+				<div className='title'>
+					<h1>Record Audio</h1>
+				</div>
+				<div className='btn-container'>
+					<button onClick={isRecording ? stopRecording : startRecording}>
+						{isRecording ? "Stop Recording" : "Start Recording"}
+					</button>
+				</div>
+			</div>
+		</main>
+	)
 }
 
-export default App;
+
+export default App
