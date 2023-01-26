5776e7fefc07e5d961c799421e7d3550efe15d3e
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2023-01-26 23:27:06 +0530

    setup restart

--- client/src/App.js
+++ client/src/App.js
@@ -1,74 +1,14 @@
-import React,{useState} from 'react'
-import {GrPlayFill,GrPauseFill} from 'react-icons/gr'
-import MicRecorder from 'mic-recorder-to-mp3';
+import React from 'react'
 
 
 const App = ()=> {
-
-	const [Mp3Recorder] = useState(new MicRecorder({ bitRate: 128 }));
-
-	const [isRecording,setIsRecording] = useState(false)
-	const [isBlocked,setIsBlocked] = useState(true)
-
-
-	// const getUserPermission = ()=> {
-	// 	navigator.mediaDevices.getUserMedia({audio:true})
-	// 		.then(()=> {
-	// 			console.log('Permission granted')
-	// 			setIsBlocked(false)
-	// 		})
-	// 		.catch(()=> {
-	// 			console.log('Permission denied')
-	// 			setIsBlocked(true)
-	// 		})
-	// }
-
-	const startRecording = ()=> {
-		// getUserPermission()
-		setIsRecording(true)
-		Mp3Recorder
-			.start()
-			.then(()=> {
-				setIsRecording(true)
-			})
-			.catch((err)=> {
-				console.log(err)
-			})
-	}
-
-	const stopRecording = ()=> {
-		setIsRecording(false)
-		Mp3Recorder
-			.stop()
-			.getMp3()
-			.then(([buffer,blob]) => {
-				const file = new File(buffer, 'audiorec.mp3', {
-					type: blob.type,
-					lastModified: Date.now()
-				});	
-				
-				console.log(file)
-			})
-			.catch((err)=> {
-				console.log(err)
-			})
-	}
-
 	return (
 		<main>
 			<div className='main-body'>
-				<div className='title'>
-					<h1>Record Audio</h1>
-				</div>
-				<div className='btn-container'>
-					<button onClick={isRecording ? stopRecording : startRecording}>
-						{isRecording ? "Stop Recording" : "Start Recording"}
-					</button>
-				</div>
+				
 			</div>
 		</main>
 	)
 }
-
 
 export default App
--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './db/connect.js';
-import mongoose from 'mongoose';
-import multer from 'multer';
-import {GridFsStorage} from 'multer-gridfs-storage'
+// import mongoose from 'mongoose';
+// import multer from 'multer';
+// import {GridFsStorage} from 'multer-gridfs-storage'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
 dotenv.config()
 
 const app = express()
 
 
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(express.json());
 
 
 // mongodb server connect
@@ -30,51 +34,51 @@ startServer()
 
 
 
-// initialising GridFs bucket
-let bucket;
-mongoose.connection.on('connected', ()=> {
-	var db = mongoose.connections[0].db
-	bucket = new mongoose.mongo.GridFSBucket(db, {
-		bucketName:'audio_files'
-	})
-})
-// initialising GridFs bucket
+// // initialising GridFs bucket
+// let bucket;
+// mongoose.connection.on('connected', ()=> {
+// 	var db = mongoose.connections[0].db
+// 	bucket = new mongoose.mongo.GridFSBucket(db, {
+// 		bucketName:'audio_files'
+// 	})
+// })
+// // initialising GridFs bucket
 
 
 
 
-// using GridFsStorage of multer-gridfs-storage to make a GridFs storage
-const storage = new GridFsStorage({
-	url:process.env.MONGO_URL,
-	file:(req,file) => {
-		return new Promise((resolve,reject) => {
-			const filename = file.originamname;
-			const fileInfo = {
-				filename:filename,
-				bucketName:'audio_files'
-			}
-			resolve(fileInfo)
-		})
-	}
-})
-// using GridFsStorage of multer-gridfs-storage to make a GridFs storage
+// // using GridFsStorage of multer-gridfs-storage to make a GridFs storage
+// const storage = new GridFsStorage({
+// 	url:process.env.MONGO_URL,
+// 	file:(req,file) => {
+// 		return new Promise((resolve,reject) => {
+// 			const filename = file.originamname;
+// 			const fileInfo = {
+// 				filename:filename,
+// 				bucketName:'audio_files'
+// 			}
+// 			resolve(fileInfo)
+// 		})
+// 	}
+// })
+// // using GridFsStorage of multer-gridfs-storage to make a GridFs storage
 
 
 
 
-// making a middleware upload having GridFs storage, to be used in /upload endpoint
-const upload = multer({
-	storage
-});
-// making a middleware upload having GridFs storage, to be used in /upload endpoint
+// // making a middleware upload having GridFs storage, to be used in /upload endpoint
+// const upload = multer({
+// 	storage
+// });
+// // making a middleware upload having GridFs storage, to be used in /upload endpoint
 
 
 
 
-app.post('/upload', upload.single('file'), (req,res) => {
-	res.status(200)
-	.send('File uploaded')
-})
+// app.post('/upload', upload.single('audio_file') ,(req,res) => {
+// 	const obj = JSON.parse(JSON.stringify(req.body)); 
+// 	console.log(obj);
+// })
 app.get('/', (req,res)=> {
     res.send('hello')
 })
